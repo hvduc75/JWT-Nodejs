4b217bbb7cbca6ccc4248b26816af5681d897201
Hoang Van Duc <duch52362@gmail.com>
2024-08-14 10:11:27 +0700

    getStarted With Boostrap 5

--- src/routes/web.js
+++ src/routes/web.js
@@ -1,11 +1,11 @@
 import express from "express";
+import homeController from "../controllers/homeController";
 
 const router = express.Router();
 
 const initWebRoutes = (app) => {
-  router.get("/", (req, res) => {
-    return res.send("hello world!");
-  });
+  router.get("/", homeController.handleHome);
+  router.get("/user", homeController.handleUser);
   return app.use("/", router);
 };
 
