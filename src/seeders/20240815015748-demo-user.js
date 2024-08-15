bdf03ab886acafe70eb78de15e0cfc85230cee65
Hoang Van Duc <duch52362@gmail.com>
2024-08-15 09:02:24 +0700

    Create Model Seeder Migration

---
+++ src/seeders/20240815015748-demo-user.js
@@ -0,0 +1,32 @@
+"use strict";
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     *
+     */
+    await queryInterface.bulkInsert(
+      "Users",
+      [
+        {
+          email: "Jonh@gmail.com",
+          password: '1234',
+          username: 'fake1',
+        },
+      ],
+      {}
+    );
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  },
+};
