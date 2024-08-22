c987942b95f9e9984b315f01090c37627ec8410c
Hoang Van Duc <duch52362@gmail.com>
2024-08-22 22:17:58 +0700

    Check Role For User

--- src/routes/api.js
+++ src/routes/api.js
@@ -2,6 +2,7 @@ import express from "express";
 import apiController from "../controllers/apiController";
 import userController from "../controllers/userController";
 import groupController from "../controllers/groupController";
+import { checkUserJWT, checkUserPermission } from "../middleware/JWTAction";
 
 const router = express.Router();
 
@@ -13,13 +14,23 @@ const testMiddleware = (req, res, next) => {
   next();
 };
 
+// const checkUserLogin = (req, res, next) => {
+//   const nonSecurePaths = ["/", "/register", "/login"];
+//   if (nonSecurePaths.includes(req.path)) return next();
+
+//   if(user) {
+//     next()
+//   } else {
+
+//   }
+// };
+
 const initApiRoutes = (app) => {
   //rest api
-  router.get("/test-api", apiController.testApi);
   router.post("/register", apiController.handleRegister);
   router.post("/login", apiController.handleLogin);
 
-  router.get("/user/read", userController.readFunc);
+  router.get("/user/read", checkUserJWT, checkUserPermission, userController.readFunc);
   router.post("/user/create", userController.createFunc);
   router.put("/user/update", userController.updateFunc);
   router.delete("/user/delete", userController.deleteFunc);
